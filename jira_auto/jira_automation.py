f0c18cf9bc046dac8281a945f066faada172a013
Ling Jin <lingjin@gmail.com>
2019-04-30 14:07:14 -0500

    Init Jira from json file

--- jira_auto/jira_automation.py
+++ jira_auto/jira_automation.py
@@ -1,6 +1,17 @@
 import json
+import base64
+from jira import JIRA
+import re
 
 with open("../jira_config.json", "r") as json_file:
     json_data = json.load(json_file)
 
-print(json_data)
+jira_server_options = json_data['jira_server_options']
+jira_usr = json_data['jira_usr']
+jira_pass = base64.b64decode(json_data['jira_pass']).decode('ascii')
+
+jira_obj = JIRA(jira_server_options, basic_auth=(jira_usr, jira_pass))
+
+projects = jira_obj.projects()
+
+print(jira_obj)
