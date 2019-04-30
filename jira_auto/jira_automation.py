2b94d12db7442dab70df20b1f997cef7258cfa2f
Ling Jin <lingjin@gmail.com>
2019-04-30 14:27:51 -0500

    Functionize

--- jira_auto/jira_automation.py
+++ jira_auto/jira_automation.py
@@ -3,15 +3,19 @@ import base64
 from jira import JIRA
 import re
 
-with open("../jira_config.json", "r") as json_file:
-    json_data = json.load(json_file)
+def init_jira_from_json():
+    with open("../jira_config.json", "r") as json_file:
+        json_data = json.load(json_file)
 
-jira_server_options = json_data['jira_server_options']
-jira_usr = json_data['jira_usr']
-jira_pass = base64.b64decode(json_data['jira_pass']).decode('ascii')
+    jira_server_options = json_data['jira_server_options']
+    jira_usr = json_data['jira_usr']
+    jira_pass = base64.b64decode(json_data['jira_pass']).decode('ascii')
 
-jira_obj = JIRA(jira_server_options, basic_auth=(jira_usr, jira_pass))
+    jira_obj = JIRA(jira_server_options, basic_auth=(jira_usr, jira_pass))
 
-projects = jira_obj.projects()
+    return jira_obj
 
-print(jira_obj)
+if __name__ == "__main__":
+    jira_obj = init_jira_from_json()
+    projects = jira_obj.projects()
+    print(projects)
