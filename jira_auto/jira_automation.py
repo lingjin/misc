e3cd35272c02d057629d2eab836c65e8bdfbe026
Ling Jin <lingjin@gmail.com>
2019-04-30 14:34:39 -0500

    Add constants

--- jira_auto/jira_automation.py
+++ jira_auto/jira_automation.py
@@ -3,14 +3,18 @@ import base64
 from jira import JIRA
 import re
 
+JIRA_CONFIG_FILE = '../jira_config.json'
+JIRA_SERVER_OPTIONS = 'jira_server_options'
+JIRA_USR = 'jira_usr'
+JIRA_PASS = 'jira_pass'
+
 def init_jira_from_json():
-    with open("../jira_config.json", "r") as json_file:
+    with open(JIRA_CONFIG_FILE, 'r') as json_file:
         json_data = json.load(json_file)
 
-    jira_server_options = json_data['jira_server_options']
-    jira_usr = json_data['jira_usr']
-    jira_pass = base64.b64decode(json_data['jira_pass']).decode('ascii')
-
+    jira_server_options = json_data[JIRA_SERVER_OPTIONS]
+    jira_usr = json_data[JIRA_USR]
+    jira_pass = base64.b64decode(json_data[JIRA_PASS]).decode('ascii')
     jira_obj = JIRA(jira_server_options, basic_auth=(jira_usr, jira_pass))
 
     return jira_obj
