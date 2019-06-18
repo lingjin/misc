554e56337b2d5ecdf9dc26ac1bed5d89ac10cc6f
Ling Jin <lingjin@gmail.com>
2019-06-18 17:32:04 -0500

    test

--- crawler/shicimingju.py
+++ crawler/shicimingju.py
@@ -12,3 +12,17 @@ if __name__ == "__main__":
     chapter = re.findall('href="/book/.{0,30}\d\.html">(.*?)</a>',html,re.S)
     bookurl = re.findall('href="(/book/.{0,30}\d\.html)">',html,re.S)
     chapterUrlBegin = re.sub('.html','',indexUrl)
+
+    for i in range(0,len(bookurl)):
+        number=re.findall('/(.{1,4})\.html',bookurl[i])
+        chapterUrl=re.sub('$',"/"+number[0]+".html",chapterUrlBegin)
+        chapterHtml=urllib.request.urlopen(chapterUrl).read()
+        chapterHtml=chapterHtml.decode('utf-8','ignore')
+        chapterText=re.findall('<div id="con2".*?>(.*?)</div>',chapterHtml,re.S)
+        chapterText=re.sub('<p>','',''.join(chapterText))
+        chapterText = re.sub('</p>', '', ''.join(chapterText))
+        chapterText = re.sub('', ' ', ''.join(chapterText))
+        f=open('.../../'+"".join(book_name)+'.txt','a',encoding='utf-8')
+        f.write(chapter[i]+"\n")
+        f.write(chapterText+"\n")
+        f.close()
